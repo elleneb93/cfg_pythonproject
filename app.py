ea2a9ead19426a887c54039cc031275422490f6a
Ellen Buckley <ellenebuckley93@gmail.com>
2018-02-14 19:55:52 +0000

    add pink and change font of index.html  via app.py using app.css

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("my_first_app")
 
 @app.route("/")
 def say_hello():
-  return "hello world!"
+  return render_template("index.html") #change filename for future pages
 
 app.run(debug=True)
